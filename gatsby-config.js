510878eb9740b9a7f1ffd31b1718112704ed13f9
nascVi <vaiiho.nascimbene@gmail.com>
2019-12-10 23:34:36 +0100

    g content env vars syntaxe

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
       }
     },
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`
+    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
