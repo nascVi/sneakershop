4ec2eb77ca061f6a2b29aace4370ab9518a01b7f
nascVi <vaiiho.nascimbene@gmail.com>
2019-12-21 05:32:38 +0100

    footer social meds test link and design

--- src/components/Globals/Footer.js
+++ src/components/Globals/Footer.js
@@ -1,4 +1,8 @@
 import React from "react"
+import { IoLogoFacebook } from "react-icons/io"
+import { IoLogoInstagram } from "react-icons/io"
+import { IoLogoGithub } from "react-icons/io"
+import { IoLogoWhatsapp } from "react-icons/io"
 
 export default function Footer() {
     return (
@@ -9,6 +13,12 @@ export default function Footer() {
                         <div>
                             ©{new Date().getFullYear()} - <span className="footer-brand"> Shoes One 972</span> - At {` `}
                             <a className="footer-link" href="https://lpchamps.cf">lpchamps<span className="footer-link-ts">`</span></a> make
+                        </div>
+                        <div className="footer-soc-meds mx-auto col-5 py-2">
+                            <a href="/"><IoLogoFacebook className="footer-Fb-med  mr-3" /></a>
+                            <a href="https://www.instagram.com/shoesone972/"><IoLogoInstagram className="footer-Insta-med mr-3" /></a>
+                            <a href="/"><IoLogoGithub className="footer-Git-med mr-3" /></a>
+                            <a href="/"><IoLogoWhatsapp className="footer-Wa-med" /></a>
                         </div>
                     </div>
                 </div>
