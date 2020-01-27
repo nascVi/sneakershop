89831746e05af93946a597345477ad70725e8de1
nascVi <vaiiho.nascimbene@gmail.com>
2020-01-27 16:15:34 +0100

    Infos button

--- src/components/Home/Info.jsx
+++ src/components/Home/Info.jsx
@@ -12,7 +12,7 @@ export default function Info() {
             <p className="lead text-muted mb-5">
               BEL BONJOU , BIENVENUE , WELCOME  sur ShoesOne972.com, <span><i>le 1er site de destockage de sneakers en Martinique (centré)</i></span>...
             </p>
-            <Link to="/shoesone/shoesone">
+            <Link to="/shoesone">
               <button className="btn text-uppercase btn-yellow">
                 Infos
               </button>
