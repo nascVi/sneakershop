ad3a499370117e6b8251cb80c0988216d2345062
nascvi <vaiiho.nascimbene@gmail.com>
2020-02-05 23:02:53 +0100

    prebuild cleaning query

--- src/pages/cleaning-service/index.js
+++ src/pages/cleaning-service/index.js
@@ -1,10 +1,50 @@
-import React from 'react';
-import Layout from '../../components/layout'
+import React from 'react'
+import Title from '../../components/Globals/Title'
+import { StaticQuery, graphql } from 'gatsby'
+import Cleaning from '../../components/Cleaning'
+
+const getServices = graphql`
+  {
+    cleanings: allContentfulShoesOneCleaning {
+        edges {
+          node {
+            title
+            video {
+              file {
+                url
+              }
+            }
+            description {
+              description
+              id
+            }
+            image {
+              fluid(maxHeight: 250) {
+                tracedSVG
+              }
+            }
+          }
+        }
+    }
+  }
+`
 
 export default function cleaning() {
     return (
-        <Layout>
-            <h2>Here sits the Cleaning service page</h2>
-        </Layout>
-    );
+    <StaticQuery
+    query={getServices}
+    render={data => {
+        <section className="py-5">
+        <div className="container">
+          <Title title="Les Services et Techniques" />
+          <div className="row">
+            {data.cleanings.edges.map(({ node: cleaning }) => {
+              return <Cleaning key={cleaning.id} product={cleaning} />
+            })}
+          </div>
+        </div>
+      </section>
+    }}
+    />
+  )
 }
--- src/pages/maboutique/index.js
+++ src/pages/maboutique/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react"
 import Products from "../../components/Products"
 import Layout from "../../components/layout"
-
 export default class maboutique extends Component {
   render() {
     return (
