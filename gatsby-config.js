ce365e6eeffae36db6da013d4f037e8502932969
nascvi <vaiiho.nascimbene@gmail.com>
2020-05-28 09:24:03 +0200

    14 in a house to 12 ⌛

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,8 +35,8 @@ module.exports = {
       resolve: "gatsby-plugin-snipcart",
       options: {
         apiKey: process.env.SNIPCART_APIKEY,
-        js: "https://cdn.snipcart.com/themes/v3.0.12/default/snipcart.js",
-        styles: "https://cdn.snipcart.com/themes/v3.0.12/default/snipcart.css",
+        js: "https://cdn.snipcart.com/themes/v3.0.14/default/snipcart.js",
+        styles: "https://cdn.snipcart.com/themes/v3.0.14/default/snipcart.css",
         autopop: false,
         credit_cards: ["visa", "mastercard", "amex"],
       },
